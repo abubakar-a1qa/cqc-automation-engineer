d5799a4a5b1f2eca9814be021385c4d13a54d8d3
Md. Abu Bakar <a.bakar@a1qa.com>
2026-01-02 16:28:37 +0600

    Updated FileUploadUtils

--- userinterface-automation/src/main/java/utils/FileUploadUtils.java
+++ userinterface-automation/src/main/java/utils/FileUploadUtils.java
@@ -13,22 +13,24 @@ public final class FileUploadUtils {
             StringSelection ss = new StringSelection(absolutePath);
             Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
 
+            Thread.sleep(1000);
+
             Robot robot = new Robot();
-            robot.setAutoDelay(200);
+            robot.setAutoDelay(100);
 
             robot.keyPress(KeyEvent.VK_CONTROL);
             robot.keyPress(KeyEvent.VK_V);
             robot.keyRelease(KeyEvent.VK_V);
             robot.keyRelease(KeyEvent.VK_CONTROL);
 
-            robot.setAutoDelay(200);
+            robot.delay(500);
 
             robot.keyPress(KeyEvent.VK_ENTER);
             robot.keyRelease(KeyEvent.VK_ENTER);
 
-            Thread.sleep(500);
+            robot.delay(1000);
         } catch (AWTException | InterruptedException e) {
-            throw new RuntimeException("Failed to upload file via explorer", e);
+            throw new RuntimeException("Failed to upload file via explorer: " + e.getMessage(), e);
         }
     }
 }
